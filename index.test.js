63b9d7116457b43189954a54ec970d829c3dc304
Connor Brathwaite <connor.brathwaite@gmail.com>
2018-02-06 10:14:25 -0500

    feat(main + tests): test
    false
    
    spent: 30m
    
    issues: #5
    
    transition: resolve

--- index.test.js
+++ index.test.js
@@ -1,23 +1,15 @@
-const sinon = require('sinon')
-const chai = require('chai')
-const sinonChai = require('sinon-chai')
-const expect = chai.expect
-chai.use(sinonChai)
+const adapter = require('./index.js')
 
-const czJiraSmartCommit = require('./index.js')
-
-describe('prompt for inputs', () => {
-  it('should be a function', () => {
-    expect(czJiraSmartCommit.prompter).to.be.a('function')
-  })
+it('should be a function', () => {
+  expect(adapter.prompter).toBeInstanceOf(Function)
 })
 
 describe('format commits', () => {
   const type = 'feat'
   const scope = 'chore'
   const isBreaking = false
-  const transition = 'closed'
-  const issues = 'CZ-234 CZ-235'
+  const transition = 'done'
+  const issues = '#1, #2'
   const time = '3y 2w 7d 8h 30m'
   const message = 'sample commit message'
   const subject = 'bumped react dependency to 16.0.2rc'
@@ -34,34 +26,11 @@ describe('format commits', () => {
   }
 
   it('should be a function', () => {
-    expect(czJiraSmartCommit.formatCommit).to.be.a('function')
+    expect(adapter.formatCommit).toBeInstanceOf(Function)
   })
 
   it('should perform a full commit', () => {
-    const result = czJiraSmartCommit.formatCommit(mockCommit)
-    expect(result).to.equal(
-      'feat(chore): bumped react dependency to 16.0.2rc CZ-234 CZ-235 #transition closed #time 3y 2w 7d 8h 30m'
-    )
-  })
-
-  it('should commit without a workflow', () => {
-    const result = czJiraSmartCommit.formatCommit(mockCommit)
-    expect(result).to.equal(
-      'feat(chore): bumped react dependency to 16.0.2rc CZ-234 CZ-235 #transition closed #time 3y 2w 7d 8h 30m'
-    )
-  })
-
-  it('should commit without a time', () => {
-    const result = czJiraSmartCommit.formatCommit(mockCommit)
-    expect(result).to.equal(
-      'feat(chore): bumped react dependency to 16.0.2rc CZ-234 CZ-235 #transition closed #time 3y 2w 7d 8h 30m'
-    )
-  })
-
-  it('should commit without a comment', () => {
-    const result = czJiraSmartCommit.formatCommit(mockCommit)
-    expect(result).to.equal(
-      'feat(chore): bumped react dependency to 16.0.2rc CZ-234 CZ-235 #transition closed #time 3y 2w 7d 8h 30m'
-    )
+    const result = adapter.formatCommit(mockCommit)
+    expect(result).toMatchSnapshot()
   })
 })
